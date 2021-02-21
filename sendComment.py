79ca1b7ce18d46620779abe164b49aadc162d2a7
“readmeh” <m15035653028@163.com>
2021-02-22 04:50:12 +0800

    Initial commit

--- sendComment.py
+++ sendComment.py
@@ -109,7 +109,7 @@ def saveComment(AId, BvId,message):
     response = requests.post(url, data=dat, headers=headers)
     print(response)
     response_json = response.json()
-    print(response_json['data']['rpid_str'])
+    return response_json['data']['rpid_str']
 
 def createComment(AId, videoJson):
     #生成评论
@@ -172,6 +172,7 @@ def jizhaoanBot():
 def getRepply():
     url = "https://api.bilibili.com/x/msgfeed/reply"
 
+
     headers = {
         "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
         "Accept-Encoding":"gzip, deflate, br",
@@ -187,12 +188,42 @@ def getRepply():
     }
 
     response = requests.get(url = url, headers = headers)
-    print(response.json()['data']['items'][0],
+    print('replyid',response.json()['data']['items'][0]['id'],
           '\nvid',response.json()['data']['items'][0]['item']['subject_id'],
           '\nrid',response.json()['data']['items'][0]['item']['root_id'],
           '\nsid',response.json()['data']['items'][0]['item']['source_id'],
           '\nmsg',response.json()['data']['items'][0]['item']['source_content'])
 
+def SupplementDataReply(aid,BvId,rid,message):
+    url = 'https://api.bilibili.com/x/v2/reply/add'
+    Referer = "https://www.bilibili.com/video/{}".format(BvId)
+
+    headers = {
+        "Accept":"application/json, text/javascript, */*; q=0.01",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+        "Connection": "keep-alive",
+        "Cookie": "finger=-1486130818; buvid3=A73A4074-CD1F-8C6C-BE4B-6CB897E01AB538498infoc; _uuid=8EA2DB3A-7AB3-8FA6-A948-D20D2A5237B441478infoc; CURRENT_FNVAL=80; blackside_state=1; sid=bd2t5r8m; fingerprint=534d613ffdc5588fd5b9c4971a48a088; buvid_fp=A73A4074-CD1F-8C6C-BE4B-6CB897E01AB538498infoc; buvid_fp_plain=17BBB8B4-07AE-4FD1-BE12-D0156A1763DA143073infoc; SESSDATA=67e154bc%2C1629303472%2C26000%2A21; bili_jct=1e5ddb99c3719d05d3f7990b87f8ffb4; DedeUserID=269850875; DedeUserID__ckMd5=48fd6883b751d4ac; rpdid=|(um)Rl~kRY)0J'uYuJ~lu~R|; LIVE_BUVID=AUTO8316137519418592; PVID=3; CURRENT_QUALITY=0; bfe_id=5db70a86bd1cbe8a88817507134f7bb5",
+        "DNT": "1",
+        "Host": "api.bilibili.com",
+        "Referer": Referer,
+        "TE": "Trailers",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0"
+    }
+
+    createRequest = {
+        "oid":aid,
+        "type":"1",
+        "root":rid,
+        "parent":rid,
+        "message":message,
+        "plat":"1",
+        "ordering":"heat",
+        "jsonp":"jsonp",
+        "csrf":"1e5ddb99c3719d05d3f7990b87f8ffb4"
+    }
+
+    response = requests.post(url=url, headers=headers, data=createRequest)
 
 if __name__ == '__main__':
     #sendRoll(your_msg="hhh")
@@ -205,6 +236,12 @@ if __name__ == '__main__':
     #downloadCover(cover_image_url=NewVideoJson['data']['list']['vlist'][0]['pic'])
 
     #jizhaoanBot()
+
+    SupplementDataReply(aid=629365002,
+                        BvId="BV1Sb4y1R7ju",
+                        rid=4163841497,
+                        message="封面图：//i0.hdslb.com/bfs/archive/42ff067df7e2364ef581efeb908fade9f42a3f2c.jpg")
+
     """,
 卧龙寺的计数工程就到这里吧
 视频总数：30888
